01d6af1ca2927f1de8a0486e74df397d752e07af
filwas <fwas.corp@gmail.com>
2024-08-15 19:10:49 +0200

    can only edit your own forms

--- src/app/[formId]/edit/page.tsx
+++ src/app/[formId]/edit/page.tsx
@@ -3,6 +3,9 @@ import { getFormDataByFormID } from "@/app/db";
 import DynamicForm from "@/components/DynamicForm/DynamicForm";
 import { useDynamicFormContext } from "@/context/DynamicFormContext";
 import { FormType } from "@/types/types";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { isNull } from "drizzle-orm";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 interface FormIdPageProps {
@@ -14,13 +17,20 @@ interface FormIdPageProps {
 const EditPage = ({ params }: FormIdPageProps) => {
   const { dynamicForm, setDynamicForm } = useDynamicFormContext();
   const formID = params.formId;
+  const {userId, isSignedIn} = useAuth()
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     const fetchFormData = async () => {
       try {
-        const formData = await getFormDataByFormID(formID);
+        const formData = await getFormDataByFormID(formID);       
+        if (formData[0].userId && userId !== formData[0].userId.toString()) {
+            
+            setError(new Error("You're trying to edit a form which does not belong to you."))
+            setLoading(false)
+            return;
+        }
         setDynamicForm(formData[0].formData as FormType);
         setLoading(false);
       } catch (err) {
@@ -30,7 +40,7 @@ const EditPage = ({ params }: FormIdPageProps) => {
     };
 
     fetchFormData();
-  }, [formID, setDynamicForm]);
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
