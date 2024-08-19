8319d4614cb7b79553db46918bf9dc83c5ee3277
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-19 15:37:56 +0200

    feat: add links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,6 +23,7 @@ const footerLinks = [
 ];
 
 export default function Footer() {
+  const pathname = usePathname()
   const [year, setYear] = useState(new Date().getFullYear());
   useEffect(() => {
     // Update the year when the component mounts
@@ -32,7 +33,19 @@ export default function Footer() {
     <footer className="mt-auto text-[#94A3B8] border-t text-center text-sm py-14 w-full">
       <div className="w-[280px] mx-auto leading-6 flex-col">
         <div>&copy; {year} Fromulatrix | All Rights Reserved</div>
-        <ul className="flex gap-x-5 text-[14px]">Filip | Julia | Viera | Gosia</ul>
+        <ul className="flex gap-x-5 text-[14px] justify-between align-center">
+          {footerLinks.map((link) => (
+            <li key={link.href}>
+              <Link
+                className={`${
+                  pathname === link.href ? "text-slate-900" : "text-slate-400"
+                }`}
+                href={link.href}
+              ></Link>
+              {link.label}
+            </li>
+          ))}
+        </ul>
       </div>
     </footer>
   );
