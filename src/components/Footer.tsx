f21b77e070a9295df7c62211bf3ac298a33cabff
transcendence12 <gosiek.krawczuk@gmail.com>
2024-08-19 15:47:10 +0200

    style: links on hover and focus in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   const [year, setYear] = useState(new Date().getFullYear());
   useEffect(() => {
     // Update the year when the component mounts
@@ -33,16 +33,17 @@ export default function Footer() {
     <footer className="mt-auto text-[#94A3B8] border-t text-center text-sm py-14 w-full">
       <div className="w-[280px] mx-auto leading-6 flex-col">
         <div>&copy; {year} Fromulatrix | All Rights Reserved</div>
-        <ul className="flex gap-x-5 text-[14px] justify-between align-center">
+        <ul className="flex gap-x-3 text-[14px] justify-between align-center">
           {footerLinks.map((link) => (
             <li key={link.href}>
               <Link
-                className={`${
-                  pathname === link.href ? "text-slate-900" : "text-slate-400"
-                }`}
                 href={link.href}
-              ></Link>
-              {link.label}
+                className={`${
+                  pathname === link.href ? "text-slate-900" : "text-[#94A3B8]"
+                }  hover:text-blue-600 focus:text-blue-600`}
+              >
+                {link.label}
+              </Link>
             </li>
           ))}
         </ul>
